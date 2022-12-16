df7c565e4719d15292bd86dadae38ebffbb24da6
felipe-tneves <felipe.tadeusantos1@gmail.com>
2022-12-16 20:07:08 -0300

    Login Angular

---
+++ src/app/views/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { User } from 'src/app/models/user';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+ userModel = new User("","")
+
+ receberDados(){
+  console.log(this.userModel)
+ }
+}
